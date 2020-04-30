c8ce617eccc906171afc4b737a6dc5fe1462349d
aadilsaifi8368 <64583763+aadilsaifi8368@users.noreply.github.com>
2020-04-30 17:15:04 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 import os
 class my():
-    BOT_TOKEN = os.environ["BOT_TOKEN"]    # from @botfather
-    API_ID = int(os.environ["API_ID"])     # from https://my.telegram.org/apps
-    API_HASH = os.environ["API_HASH"]      # from https://my.telegram.org/apps
+    BOT_TOKEN = os.environ["1167093990:AAHPZz8Rv7bp1-AJrU_NrWNZ-dwQ64qc8R4"]    # from @botfather
+    API_ID = int(os.environ["1345809"])     # from https://my.telegram.org/apps
+    API_HASH = os.environ["dceb6fa6f881886f19283916cd601678"]      # from https://my.telegram.org/apps
